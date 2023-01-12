8e5cc23372126af11e891c404387569570618bf9
Buddhabhushan Dnyandeo Bhagat <m1067112@mindtree.com>
2023-01-12 23:37:11 +0530

    Added Testcases for Transaction service

--- src/test/java/com/fundtransfer/service/TransactionServiceImplTest.java
+++ src/test/java/com/fundtransfer/service/TransactionServiceImplTest.java
@@ -1,21 +1,23 @@
 package com.fundtransfer.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.fundtransfer.entity.Transaction;
 import com.fundtransfer.repository.TransactionRepository;
 
+@ExtendWith(MockitoExtension.class)
 class TransactionServiceImplTest {
 
 	
@@ -34,12 +36,27 @@ class TransactionServiceImplTest {
 
 	@Test
 	void testSaveTransaction() {
-		
-	}
+		Transaction transaction = new Transaction(Math.abs(new Random().nextLong()),Math.abs(new Random().nextLong()),Math.abs(new Random().nextLong()),344.98,"mock summery",44.4,455.54,"sucess",new Date(),new Date(),new Date());
+		Mockito.when(transactionRepository.save(transaction)).thenReturn(transaction);
+		Transaction actual = transactionServiceImpl.saveTransaction(transaction);
+		assertEquals(transaction, actual);
+		}
 
 	@Test
 	void testGetTransactionBetweenDates() {
+		Transaction transaction1 = new Transaction(Math.abs(new Random().nextLong()),Math.abs(new Random().nextLong()),Math.abs(new Random().nextLong()),344.98,"mock summery",44.4,455.54,"sucess",new Date(),new Date(),new Date());
+		Transaction transaction2 = new Transaction(Math.abs(new Random().nextLong()),Math.abs(new Random().nextLong()),Math.abs(new Random().nextLong()),344.98,"mock summery",44.4,455.54,"sucess",new Date(),new Date(),new Date());
+		Transaction transaction3 = new Transaction(Math.abs(new Random().nextLong()),Math.abs(new Random().nextLong()),Math.abs(new Random().nextLong()),344.98,"mock summery",44.4,455.54,"sucess",new Date(),new Date(),new Date());
+		List<Transaction> transactionList = new ArrayList<>();
+		transactionList.add(transaction1);
+		transactionList.add(transaction2);
+		transactionList.add(transaction3);
 		
+        final Date date = Mockito.mock(Date.class);
+		Mockito.when(transactionRepository.findByTransactionDateBetween(date,date)).thenReturn(transactionList);
+		List<Transaction> actualTransactionList = transactionServiceImpl.getTransactionBetweenDates(date,date);
+	    assertEquals(transactionList, actualTransactionList);
+
 	}
 
 }
